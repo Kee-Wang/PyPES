d114d2ebd6ebac8c2d37cf1318489febf1b226c6
Qingfeng Wang <kee@Qingfengs-MacBook-Pro.local>
2017-02-06 15:25:52 -0500

    ia

--- pypes/plot.py
+++ pypes/plot.py
@@ -19,13 +19,15 @@ class plot():
         plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
     def version(self):
-        print('Plot Version: 0.0.4. --By Kee')
+        print('Plot Version: 0.0.5 --By Kee')
         return None
-    def heatmap(self,file,bin=100,xtitle='xtitle',ytitle='ytitle',ztitle='ztitle',title=None,save=None):
+    def heatmap(self,file=None,bin=100,xtitle='xtitle',ytitle='ytitle',ztitle='ztitle',title=None,save=None):
         """This is to construct 3d heat map. """
         import matplotlib.pyplot as plt
         import numpy as np
         self.version()
+
+
         (x, y, z) = np.loadtxt(file, unpack=True)
         xmin = x.min()
         xmax = x.max()
@@ -64,7 +66,7 @@ class plot():
 
         plt.show()
         return None
-    def line(self,file,col=[0,1],xtitle='xtitle',ytitle='ytitle',title=' ',save=None,linewidth=2):
+    def line_back(self,file,col=[0,1],xtitle='xtitle',ytitle='ytitle',title=' ',save=None,linewidth=2):
         """This is to plot simple scatter plot for n columns. order in the first column will be take as x, all other columns are taken as y"""
         import matplotlib.pyplot as plt
         import numpy as np
@@ -73,6 +75,26 @@ class plot():
         fig = plt.figure()
         ax = fig.add_subplot(111)
         data = np.loadtxt(file, unpack=True) #Read columns
+        for i in col:
+            if i == 0:
+                continue
+            plt.plot(data[0],data[i],linewidth=linewidth)
+        #ax.set_title(title)
+        ax.set_xlabel(xtitle)
+        ax.set_ylabel(ytitle)
+        self.save(fig, save)
+
+        return None
+    def line(self,file=None,data=None,col=[0,1],xtitle='xtitle',ytitle='ytitle',title=' ',save=None,linewidth=2):
+        """This is to plot simple scatter plot for n columns. order in the first column will be take as x, all other columns are taken as y"""
+        import matplotlib.pyplot as plt
+        import numpy as np
+
+        self.version()
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        if file is not None:
+            data = np.loadtxt(file, unpack=True) #Read columns
         for i in col:
             if i == 0:
                 continue
