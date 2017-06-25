62c5f49e26ecbe7c43aa20319b01d04169541695
Qingfeng Wang <kee@Qingfengs-MacBook-Pro.local>
2017-06-25 15:33:12 -0400

    all added

--- pypes/plot.py
+++ pypes/plot.py
@@ -217,7 +217,7 @@ class plot():
         return None
 
 
-    def hist(self, file, ,bin='auto', xtitle='xtitle', ytitle='ytitle', title=' '):
+    def hist(self, file,bin='auto', xtitle='xtitle', ytitle='ytitle', title=' '):
         """This is to plot simple scatter plot for n columns. order in the first column will be take as x, all other columns are taken as y"""
         import matplotlib.pyplot as plt
         import numpy as np
