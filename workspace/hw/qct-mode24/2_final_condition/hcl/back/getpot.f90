315ca082b46326e620cd024baa54348b83e685e1
Kee-Wang <kee@macronode.chem.emory.edu>
2018-04-08 15:48:24 -0400

    all added

---
+++ workspace/hw/qct-mode24/2_final_condition/hcl/back/getpot.f90
@@ -0,0 +1,348 @@
+program main
+! This program is for HWWW -> H + WWW reaction. |J| for HCl will be calculated
+! and ZPE will be checked.
+
+use final_condition
+  use pes_shell
+  use constants
+  use angular
+  implicit none
+real,parameter::Be_HCl=10.59341 !cm-1
+real,parameter::aums=2187695.176776!1au = aums m/s
+real,parameter::zpe_hcl=1483.743446 !From DMC
+real,parameter::zpe_www=15602.536472 !From DMC
+real,parameter::zpe_hwww = 17683.110522 !DMC = 17683.110522
+
+real,parameter::E_hcl_ref =8463.2867115594708594 
+real,parameter::E_www_ref =2796.6585222145845364 
+real,parameter::E_given =  21233.1103893624
+real,dimension(:,:),allocatable::xx,gradd,velo
+character(len=2),dimension(:),allocatable::sym
+character(len=32)::filename
+integer::k,i,natm,ierr,j,vio
+real::pot,diss, E_trans,E_T, E_V,  M_hcl, E_rot, speed, Evib, Erot_hcl
+real::speed_www, Erot_www,Evib_www
+real,dimension(:),allocatable::mass
+integer::ab_j
+real,dimension(:),allocatable::x
+real::j_temp
+real,dimension(27)::x_www
+real,dimension(27)::v_www
+real,dimension(6)::x_hcl
+real,dimension(6)::v_hcl
+real::x_hwww(33), v_hwww(33), kin_hwww, E_tot, D0
+real::abc(3), abc_www(3), D0count(4), red_w, red_h, pot_h, pot_www
+real::vcom_h(3),vcom_www(3)
+
+!For HCl fragment
+real,dimension(2)::mass_hcl !Redefine mass for hcl
+real,dimension(9)::mass_www !Redefine mass for www
+
+real,dimension(3,9)::www_r, www_v !Coordiante for hcl and velocity for hcl
+real,dimension(3,9)::www_r_prime, www_v_prime !Coordiante and velocity in com
+
+real,dimension(3,2)::hcl_r, hcl_v !Coordiante for hcl and velocity for hcl
+real,dimension(3,2)::hcl_r_prime, hcl_v_prime !Coordiante and velocity in com
+
+real,dimension(3)::www_com_r, www_com_v!Vectos to define com coord and velocity
+real,dimension(3)::j_www, omega_www !Angular momentum of www
+
+real,dimension(3)::hcl_com_r, hcl_com_v!Vectos to define com coord and velocity
+real,dimension(3)::j_hcl, omega, j_hcl_calc !Angular momentum of HCl
+real::j_calc
+real,dimension(2,3)::xxx,vv
+real::Erot,J1,J1_sum,J2,J2_sum,J3,J3_sum,com_velc(3), kine_h, kine_www
+integer::count1,count2,count3
+integer::Jcount(4),iflag(4),Jrot(4),Jsum(4),vsum(4),J4count, J6count
+real::miu,Et,Etsum(4), Etj4, Etj6, speedj4,speedj6
+!real::speed(4)
+Jcount = 0
+Jsum = 0
+Jrot = 0
+vsum=0
+D0count = 0
+Etsum=0
+J4count =0
+j6count = 0
+speedj4 = 0
+speedj6= 0
+
+  call getarg(1,filename)
+  open(21,status='old',file=filename)
+  i=index(filename,'.',.true.)
+  filename=filename(1:i-1)
+  open(34,status='unknown',file=trim(filename)//"_s4.geom")!Hard restriction
+
+
+  open(22,status='unknown',file=trim(filename)//"_s1.txt")!No restriction
+  open(23,status='unknown',file=trim(filename)//"_s2.txt")!HCl restriction
+  open(24,status='unknown',file=trim(filename)//"_s3.txt")!Soft restriction
+  open(25,status='unknown',file=trim(filename)//"_s4.txt")!Hard restriction
+
+  open(26,status='unknown',file=trim(filename)//"_s1_j4.txt")!J=4
+  open(27,status='unknown',file=trim(filename)//"_s2_j4.txt")
+  open(28,status='unknown',file=trim(filename)//"_s3_j4.txt")
+  open(29,status='unknown',file=trim(filename)//"_s4_j4.txt")
+  open(30,status='unknown',file=trim(filename)//"_s1_j6.txt")!J=6
+  open(31,status='unknown',file=trim(filename)//"_s2_j6.txt")
+  open(32,status='unknown',file=trim(filename)//"_s3_j6.txt")
+  open(33,status='unknown',file=trim(filename)//"_s4_j6.txt")
+
+!write(22,*) '# No ZPE restriction, Energy (cm-1), speed (m/s)'
+
+!write(23,*) '# HCl ZPE restriction,  Energy (cm-1), speed (m/s)'
+!write(24,*) '# Soft ZPE restriction, Energy (cm-1), speed (m/s)'
+!write(25,*) '# Hard ZPE restriction,  Energy (cm-1), speed (m/s)'
+write(22,'(3A15)')  '#No_Constraint','#No_Constraint','#No_Constraint'
+write(23,'(3A15)')  '#Hard_on_HCl', '#Hard_on_HCl','#Hard_on_HCl'
+write(24,'(3A15)')  '#Soft_ZPE','#Soft_ZPE','#Soft_ZPE'
+write(25,'(3A15)')  '#Hard_ZPE','#Hard_ZPE','#Hard_ZPE'
+
+write(26,'(3A15)')  '#No_Constraint','#No_Constraint','#No_Constraint'
+write(27,'(3A15)')  '#Hard_on_HCl', '#Hard_on_HCl','#Hard_on_HCl'
+write(28,'(3A15)')  '#Soft_ZPE','#Soft_ZPE','#Soft_ZPE'
+write(29,'(3A15)')  '#Hard_ZPE','#Hard_ZPE','#Hard_ZPE'
+
+write(30,'(3A15)')  '#No_Constraint','#No_Constraint','#No_Constraint'
+write(31,'(3A15)')  '#Hard_on_HCl', '#Hard_on_HCl','#Hard_on_HCl'
+write(32,'(3A15)')  '#Soft_ZPE','#Soft_ZPE','#Soft_ZPE'
+write(33,'(3A15)')  '#Hard_ZPE','#Hard_ZPE','#Hard_ZPE'
+
+do j=1,12
+!write(21+j,*) '#HCl ZPE (DMC): 1483.743446, WWW ZPE (DMC): 15602.536472'
+!write(21+j,*) '#E_given = DMC ZPE + 3550 =21233.1103893624.'
+!write(21+j,*) '#Dvib = Evib - ZPE'
+!write(21+j,*) "#D0 = E_given - Erot(W) - Erot(HWW) - Evib(W) + Evib(HWW) -E_COM,&
+! E_COM=0"
+!write(21+j,*)
+!For self readiing
+!write(21+j,'(8A15,A2)')  '#Erot(HCl)','#Erot(WWW)',&
+!'#Dvib(HCl)', '#Dvib(WWW)','D0', '#Speed','#J'
+!For export
+write(21+j,'(3A15)')  '#Erot(HCl)cm-1','#J','#Speed(m/s)'
+!write(21+j,'(3A15)')  '#Erot(HCl) (cm-1)','#J','#Speed(m/s)'
+
+
+end do
+
+  do i=1,4
+end do
+  read(21,*) natm
+  read(21,*) 
+
+  allocate(xx(3,natm))
+  allocate(gradd(3,natm))
+  allocate(velo(3,natm))
+  allocate(sym(natm))
+  allocate(mass(natm))
+  allocate(x(natm*3))
+
+  do i=1,natm
+     read(21,*) sym(i),xx(:,i),gradd(:,i),velo(:,i)
+       select case(sym(i))
+       case("C")
+          mass(i)=c_mass
+       case("H")
+          mass(i)=h_mass
+       case("O")
+          mass(i)=o_mass
+       case("Cl")
+          mass(i)=cl_mass
+       end select
+    end do
+  rewind(21)
+mass = mass*emass
+
+call hcl_init(1)
+call wat_init(3)
+
+  k = 0!record total number of configs
+  do
+     read(21,*,iostat=ierr) natm
+     if (ierr.ne.0) exit
+     k = k + 1
+     read(21,*)
+     do i=1,natm
+        read(21,*) sym(i),xx(:,i),gradd(:,i),velo(:,i)!(i*3-2:i*3)
+     end do
+     xx = xx/auang !xx in Bohr
+
+!For whole
+do j=1,11
+x_hwww(3*j-2:3*j) = xx(:,j)
+v_hwww(3*j-2:3*j) = velo(:,j)
+end do
+
+
+
+
+! Kinetic energy of HWWW, this should be 0
+com_velc=0
+
+    do i=1,natm
+       com_velc=com_velc+mass(i)*v_hwww(3*i-2:3*i)
+    end do
+com_velc=com_velc/sum(mass)
+!kin_hwww = calc_kine(mass,com_velc)
+kin_hwww = calc_kine(mass, com_velc) * aucm
+
+call xcom(mass,x_hwww)
+call vcom(mass,v_hwww)
+
+
+
+!Assign cooridnate and velocity to hcl, as 1d
+do j=1,2
+x_hcl(3*j-2:3*j) = x_hwww(3*(9+j)-2:3*(9+j))
+v_hcl(3*j-2:3*j) = v_hwww(3*(9+j)-2:3*(9+j))
+end do
+
+!For water
+do j=1,9
+x_www(3*j-2:3*j) = x_hwww(3*j-2:3*j)
+v_www(3*j-2:3*j) = v_hwww(3*j-2:3*j) 
+end do
+
+
+
+!Calculate final condition of HCl
+
+call fin_cond(mass(10:11),x_hcl,v_hcl,speed,kine_h, Erot,j_hcl,vcom_h)
+call fin_cond(mass(1:9),x_www,v_www,speed_www, kine_www, Erot_www,j_www,vcom_www)
+miu = 1/(1/sum(mass(1:9))+1/sum(mass(10:11)))
+Erot_hcl = Erot
+!j_hcl_calc = &
+!vec_cross(x_hcl(1:3),v_hcl(1:3)*mass(10))+vec_cross(x_hcl(4:6),v_hcl(4:6)*mass(11))
+!j_calc = sqrt(sum(j_hcl_calc**2))
+!Be_HCl
+
+! Calculate the rotation constant |J|
+
+!ab_j = nint(sqrt(0.25 + ab_j)-0.5)
+ab_j = nint(sqrt(sum(j_hcl**2))-0.5) !Another way to get ab_j
+ab_j = nint(sqrt(0.25 + sum(j_hcl**2))-0.5) !A way to get quntum J
+!write(*,*) ab_j,j_calc
+
+
+!j_calc = sqrt(0.25 + ab_j)-0.5
+!write(*,*) 'compare', ab_j
+Erot = ab_j*(ab_j+1)*Be_HCl 
+!write(*,*) Erot*aucm,j_calc*(j_calc+1)*Be_HCl
+
+
+!calculate vibrational energy, so that to determin if zpe violat
+nwat = 0
+nhcl = 1!Calculate 1 hcl only
+pot_h = f(xx(:,10:11))*aucm - E_hcl_ref
+Evib =calc_kine(mass(10:11),v_hcl) - Erot/aucm
+Evib = Evib*aucm + pot_h!(f(xx(:,10:11))*aucm - E_hcl_ref)
+!For www
+nwat = 3!Calculate water trimer
+nhcl = 0
+pot_www = f(xx(:,1:9))*aucm - E_www_ref
+Evib_www =calc_kine(mass(1:9),v_www) - Erot_www
+Evib_www = Evib_www*aucm + pot_www!(f(xx(:,1:9))*aucm - E_www_ref)
+
+!if (k .eq. 3) then
+!stop
+!end if
+
+!Claasification
+iflag = 0
+iflag(1) = 1
+if (Evib > zpe_hcl) then !Only HCl
+iflag(2) = 1
+end if
+if (Evib + Evib_www > zpe_www + zpe_hcl) then !Soft ZPE
+iflag(3) = 1
+!write(*,*) kine_h*aucm*sum(mass(:))/(sum(mass(:))-sum(mass(10:11))),kine_h*aucm+ kine_www*aucm
+!write(*,*) kin_hwww,kine_h*aucm+kine_www*aucm, kine_h*aucm! kine_www*aucm
+!write(*,*) E_given, Erot_hcl, Erot_www, pot_h, pot_www
+!write(*,*) E_given, (Erot_hcl+ Erot_www)*aucm+ pot_h+ pot_www
+!write(*,*) E_given - (Erot_hcl + Erot_www)*aucm - pot_h - pot_www
+end if
+if (Evib > zpe_hcl .and. Evib_www > zpe_www) then !Hard ZPE
+iflag(4) = 1
+
+
+end if
+
+!Rotational energy
+!Erot = Erot * aucm
+Erot_www = Erot_www * aucm
+Erot_hcl = Erot_hcl * aucm
+
+!Excessive vibrational energy
+red_w = Evib_www - zpe_www
+red_h = Evib - zpe_hcl
+
+
+!ikin_hwww = kin_hwww * aucm
+D0 = E_given - pot_h - pot_www -  kin_hwww
+!write(*,*) D0
+!D0 = E_given-zpe_hwww   - red_h - red_w-Erot-Erot_www&
+!-kine_h-kine_www
+! Write files for different J condition
+do i = 1,4
+if (iflag(i) .eq. 1)  then!not violate zpe of hcl
+Jsum(i) = Jsum(i) + ab_j!
+Jcount(i) = Jcount(i) + 1
+vsum(i) = vsum(i) + speed*aums
+D0count(i) = D0count(i) + D0
+Et = 0.5*miu*sum((vcom_h-vcom_www)**2)*aucm
+!write(*,*) Et
+!write(*,*) E_given - pot_h - pot_www - (Erot_www + Erot_hcl) - kin_hwww
+!write(*,*) 0.5*miu*sum((vcom_h-vcom_www)**2)*aucm
+Etsum(i) = Etsum(i) + 0.5*miu*sum((vcom_h-vcom_www)**2)*aucm
+!Record
+! Record overal speed distrubtion and j distribution
+!write(21+j,'(3A15)')  '#Erot(HCl) (cm-1)','#J','#Speed(m/s)'
+!For export
+write(21+i,'(F15.2,I15,F15.2)')  Erot,ab_j, speed*aums
+
+!For selfuse
+!write(21+i,'(6(F15.2),I15)')  Erot, Erot_www, &
+!Evib - zpe_hcl, Evib_www-zpe_www, D0, speed*aums, ab_j
+
+!For J=4 configs
+if (abs(ab_j-4.0) <=1d-5) then
+write(25+i,'(F15.2,I15,F15.2)')  Erot,ab_j, speed*aums
+if (iflag(3) .eq. 1 .and. i .eq. 3) then
+write(*,*) J4count
+J4count = J4count + 1
+Etj4 = Etj4 + Et
+speedj4 = speedj4 + speed*aums
+end if
+!write(25+i,'(6(F15.2),I15)')  Erot, Erot_www, &
+!Evib - zpe_hcl, Evib_www-zpe_www, D0, speed*aums, ab_j
+end if
+
+!For J=6 configs
+if (abs(ab_j-6.0) <=1d-5) then
+write(29+i,'(F15.2,I15,F15.2)')  Erot,ab_j, speed*aums
+if (iflag(3) .eq. 1 .and. i .eq. 3) then
+J6count = J6count + 1
+Etj6 = Etj6 + Et
+speedj6 = speedj6 + speed*aums
+end if
+!write(29+i,'(6(F15.2),I15)')  Erot, Erot_www, &
+!Evib - zpe_hcl, Evib_www-zpe_www, D0, speed*aums, ab_j
+end if
+
+end if
+end do
+end do
+
+write(*,*) 'Jcount,no/hard on hcl/soft/hard',Jcount
+write(*,'(A20,4F8.2)') 'Rejection Rate:',1-real(Jcount)/real(Jcount(1))
+write(*,'(A20,4F8.2)') 'Average J: ',Jsum(:)/real(Jcount(:))
+write(*,'(A20,4F8.2)') 'Average Speed (m/s): ',vsum(:)/real(Jcount(:))
+write(*,'(A20,4F8.2)') 'Average D0 (cm-1): ',D0count(:)/real(Jcount(:))
+write(*,'(A20,4F8.2)') 'Average Et (cm-1): ',Etsum(:)/real(Jcount(:))
+write(*,*) 'Et (j=4), soft ZPE:', Etj4/real(J4count)
+write(*,*) 'Et (j=6), soft ZPE:', Etj6/real(J6count)
+write(*,*) 'Speed (j=4), spft ZPE:', speedj4/real(J4count)
+write(*,*) 'Speed (j=6), spft ZPE:', speedj6/real(J6count)
+write(*,*) J4count,J6count
+
+end program main
---
+++ workspace/hw/qct-mode24/3_results/plot_all.py
@@ -0,0 +1,326 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from pypes.plot import plot
+from matplotlib import rc
+#rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+#rc('font',**{'family':'serif','serif':['Times']})
+#rc('text', usetex=True)
+#plt.rcParams.update({'font.family': 'Times'})
+plt.rc('font', family='serif')
+plt.rc('font', serif='Times New Roman')
+
+ph = '../2_final_condition/hcl/result_H_WWW'
+ph2 = '../2_final_condition/hcl/result_H_WWW'
+file_hclj4 = [ph+'_s1_j4.txt',ph+'_s2_j4.txt',ph+'_s3_j4.txt',ph+'_s4_j4.txt']
+file_hclj6 = [ph+'_s1_j6.txt',ph+'_s2_j6.txt',ph+'_s3_j6.txt',ph+'_s4_j6.txt']
+file_hcl = [ph+'_s1.txt',ph+'_s2.txt',ph+'_s3.txt',ph+'_s4.txt']
+file_expj4 = './expdata/hcl_speed_j=4.prn'
+file_expj6 = './expdata/hcl_speed_j=6.prn'
+ph = '../2_final_condition/water/result_HWW_W'
+file_w = [ph+'_s1.txt',ph+'_s2.txt',ph+'_s3.txt',ph+'_s4.txt']
+f = open('../3_results/excel/HCl_bin.txt','w')
+
+
+def hcl_j_speed_plot(file1,file2,save,ymax2,bin,index):
+    SMALL_SIZE = 12
+    MEDIUM_SIZE = 14
+    BIGGER_SIZE = 16
+    plt.rc('font', size=SMALL_SIZE)  # controls default text sizes
+    plt.rc('axes', titlesize=SMALL_SIZE)  # fontsize of the axes title
+    plt.rc('axes', labelsize=BIGGER_SIZE)  # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)  # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+    label = ['No Constraint', 'HCl only ZPE', 'Soft ZPE', 'Hard ZPE']
+    xtitle = 'Speed (m/s)\n\n'+note[0]
+    ytitle1 = 'Experiment signal intensity'
+    ytitle2 = 'Trajectory count'
+    ymax1 = 150
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    #ax.text(note[1], note[2], note[0], fontsize=18)
+    #ax.set_title(ax_title)
+    data1 = np.loadtxt(file1, unpack=True)
+    ax.set_xlabel(xtitle)
+    ax.set_ylabel(ytitle1)
+    ax.set_ylim([0, ymax1])
+    #plt.plot(data1[0],data1[1],'r-',linewidth=linewidth,label='IR ON')
+    plt.plot(data1[0],data1[index],'k-',linewidth=1,label='Exp')
+    ax.legend()
+    ax2 = ax.twinx()
+    #data2 = list()
+    ax2.set_ylim([0, ymax2])
+    ax2.set_ylabel(ytitle2)
+    for i in range(len(file2)):
+        data2 = np.loadtxt(file2[i], unpack=True)
+        y, binEdges = np.histogram(data2[2], bins=bin[i])
+        bincenters = 0.5 * (binEdges[1:] + binEdges[:-1])
+        plt.plot(bincenters,y,'-',linewidth=3,label = label[i])
+        for j in range(len(y)):
+            f.write("{:5d}   {:6.2f}\n".format(y[j],bincenters[j]))
+        f.write("\n")
+
+    ax2.legend(loc=7)
+    a = plot()
+    a.save(fig=fig, save=save)
+
+"J selected speed distribution for HCl"
+
+save = './result_hcl/j4speed.eps'
+ymax2 = 200
+bin = [40,40,40,40]
+note = ['(a)',800,140]
+hcl_j_speed_plot(file_expj4,file_hclj4,save,ymax2,bin,index=2)
+
+save = './result_hcl/j6speed.eps'
+ymax2 = 120
+#bin = [20,10,10,10]
+label = ['No Constraint','HCl only ZPE','Soft ZPE','Hard ZPE']
+note = ['(b)',800,140]
+hcl_j_speed_plot(file_expj6,file_hclj6,save,ymax2, bin,index=1)
+
+
+def hcl_j_distribution(file2,save):
+    import numpy as np
+    """This is to plot simple scatter plot for n columns. order in the first column will be take as x, all other columns are taken as y"""
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ymax1 = 1.2
+    ymax2 = 1.2
+    xtitle = 'HCl rotational energy (cm$^{-1})$'
+    ytitle1 = 'Experiment relative population'
+    ytitle2 = 'Theory (normalized at J=3)'
+    linewidth = 1
+    bin = 10
+    label = ['No Constraint', 'HCl only ZPE', 'Soft ZPE', 'Hard ZPE']
+    Be_HCl = 10.59341
+
+    ax.set_xlabel(xtitle)
+    ax.set_ylabel(ytitle1)
+    ax.set_ylim([0, ymax1])
+    x = list()
+    for num in [3, 4, 5, 6, 7, 8]:
+        x.append(num*(num+1) * Be_HCl)
+    #x = [3, 4, 5, 6, 7, 8] * Be_HCl
+    y = [1, 0.6, 0.4, 0.09, 0.034, 0.02]
+    yerr = [0.07, 0.072, 0.1, 0.08, 0.02, 0.012]
+
+    ax.errorbar(x, y, yerr=yerr, fmt='ko', capsize=5, elinewidth=3, markeredgewidth=2, label='Exp')
+    ax.legend()
+
+    ax2 = ax.twinx()
+    ax2.set_ylim([0, ymax2])
+    ax2.set_ylabel(ytitle2)
+    multi=[1,5,10,100]
+    unique = [0,0,0,0]
+    counts = [0,0,0,0]
+    loc=list()
+
+    max = list()
+    for i in range(len(file2)):
+        data2 = np.loadtxt(file2[i], unpack=True)
+        #print(data2[0])
+        #x1 = np.array(data2[0])
+        x1 = np.array(data2[1])
+        unique[i], counts[i] = np.unique(x1, return_counts=True)
+        count = 0
+
+
+        print(unique[i],counts[i])
+        plt.plot((unique[i]*(unique[i]+1)*Be_HCl),counts[i]/counts[i][3],'-',linewidth=2,label = label[i])
+    ax2.legend(loc=7)
+    a = plot()
+    a.save(fig=fig, save=save)
+    return None
+
+
+
+#hcl_j_distribution(file_hcl,save = './result_hcl/hcl_j_distribution.eps')
+
+
+
+def w_erot_plot(file2,save):
+    ytitle2 = 'Trajectory count'
+    xtitle = 'Water monomer rotational energy (cm$^{-1}$)'
+    plt.rc('xtick', labelsize=12)  # fontsize of the axes title
+    fig = plt.figure()
+    ax2 = fig.add_subplot(111)
+    label = ['No Constraint', 'Hard ZPE on monomer', 'Soft ZPE on both', 'Hard ZPE on both']
+
+    ax2.set_xlabel(xtitle)
+
+    bin = [50,50,50,20]
+    ax2.set_ylabel(ytitle2)
+    ax2.set_xlim([0,2000])
+
+    for i in range(len(file2)):
+        #f = open('water_j_distribution_s{:d}.txt'.format(i+1), 'w')
+        data2 = np.loadtxt(file2[i], unpack=True)
+
+        y, binEdges = np.histogram(data2[0], bins=bin[i])
+        bincenters = 0.5 * (binEdges[1:] + binEdges[:-1])
+        plt.plot(bincenters,y,'-',linewidth=3,label = label[i])
+        #count = 0
+        #f.write('#Bin center    #Number of count \n')
+        #for num in y:
+        #    f.write('{:f},{:d} \n'.format(binEdges[count],num))
+        #    count = count + 1
+        #f.close()
+    #plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+
+    ax2.legend(loc=7)
+    a = plot()
+    a.save(fig=fig, save=save)
+
+
+#w_erot_plot(file_w,save = './result_water/water_erot_distribution.eps')
+
+
+
+
+
+"Not used"
+
+
+def hcl_erot_plot(file2,save,ymax1,ymax2,xtitle,ytitle1,ytitle2,note,label):
+    plt.rc('xtick', labelsize=12)  # fontsize of the axes title
+    fig = plt.figure()
+    ax2 = fig.add_subplot(111)
+
+    ax2.set_xlabel(xtitle)
+
+    bin = [50,50,50,20]
+    ax2.set_ylabel(ytitle2)
+    ax2.set_xlim([0,2000])
+
+    for i in range(len(file2)):
+        #f = open('water_j_distribution_s{:d}.txt'.format(i+1), 'w')
+        data2 = np.loadtxt(file2[i], unpack=True)
+
+        y, binEdges = np.histogram(data2[0], bins=bin[i])
+        bincenters = 0.5 * (binEdges[1:] + binEdges[:-1])
+        plt.plot(bincenters,y,'-',linewidth=3,label = label[i])
+        count = 0
+
+        #f.write('#Bin center    #Number of count \n')
+        #for num in y:
+        #    f.write('{:f},{:d} \n'.format(binEdges[count],num))
+        #    count = count + 1
+
+
+        #f.close()
+    #plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+
+    ax2.legend(loc=7)
+    a = plot()
+    a.save(fig=fig, save=save)
+save = './result_hcl/hcl_erot_distribution.eps'
+label = ['No Constraint', 'Hard ZPE on monomer', 'Soft ZPE on both', 'Hard ZPE on both']
+xtitle = 'HCl monomer rotational energy (cm$^{-1}$)'
+#hcl_erot_plot(file_hcl, save, ymax1, ymax2, xtitle, ytitle1, ytitle2, note, label)
+
+
+def j_w_plot(file2,save,ymax1,ymax2,xtitle,ytitle1,ytitle2,note,label):
+    plt.rc('xtick', labelsize=20)  # fontsize of the axes title
+    fig = plt.figure()
+    ax2 = fig.add_subplot(111)
+    #ax.text(note[1], note[2], note[0], fontsize=18)
+    #ax.set_title(ax_title)
+    #data1 = np.loadtxt(file1, unpack=True)
+    ax2.set_xlabel(xtitle)
+    #ax.set_ylabel(ytitle1)
+    #ax.set_ylim([0, ymax1])
+    #plt.plot(data1[0],data1[1],'r-',linewidth=linewidth,label='IR ON')
+    #plt.plot(data1[0],data1[index1],'k-',linewidth=linewidth,label='Exp')
+    #ax.legend()
+    #ax2 = ax.twinx()
+    #data2 = list()
+    #ax2.set_ylim([0, ymax2])
+    bin = [50,50,50,20]
+    ax2.set_ylabel(ytitle2)
+
+    for i in range(len(file2)):
+        print(file2[i])
+        f = open('water_j_distribution_s{:d}.txt'.format(i+1), 'w')
+        data2 = np.loadtxt(file2[i], unpack=True)
+        y, binEdges = np.histogram(data2[0], bins=bin[i])
+        bincenters = 0.5 * (binEdges[1:] + binEdges[:-1])
+        plt.plot(bincenters,y,'-',linewidth=3,label = label[i])
+        count = 0
+        f.write('#Bin center    #Number of count \n')
+        for num in y:
+            f.write('{:f},{:d} \n'.format(binEdges[count],num))
+            count = count + 1
+
+
+        f.close()
+
+    ax2.legend(loc=7)
+    a = plot()
+    a.save(fig=fig, save=save)
+#file2 = #['./result_water/result_HWW_W.s1','./result_water/result_HWW_W.s2','./result_water/result_HWW_W.s3','./result_water/result_HWW_W.s4']
+save = 'water_j_distribution.eps'
+label = ['No Constraint','Hard ZPE on monomer','Soft ZPE on both','Hard ZPE on both']
+xtitle = 'J(water monomer)'
+#j_w_plot(file_w,save,ymax1,ymax2,xtitle,ytitle1,ytitle2,note,label)
+
+
+def joverallspped_plot2(file_hcl,save,xlim,xtitle,ytitle1,ytitle2,label,index1,index2):
+    fig = plt.figure(figsize=(6, 9))
+
+
+    #ax.set_title(ax_title)
+    #data1 = np.loadtxt(file1, unpack=True)
+    #
+    #ax.set_ylabel(ytitle1)
+    #ax.set_ylim([0, ymax1])
+    #plt.plot(data1[0],data1[1],'r-',linewidth=linewidth,label='IR ON')
+    #plt.plot(data1[0],data1[index1],'k-',linewidth=linewidth,label='Exp')
+    #ax.legend()
+    #ax2 = ax.twinx()
+    #data2 = list()
+    #ax2.set_ylim([0, ymax2])
+    num = 410
+    for i in range(len(file2)):
+        num = num+1
+        ax = fig.add_subplot(num)
+        ax.set_ylabel(ytitle2)
+        data2 = np.loadtxt(file2[i], unpack=True)
+        y, binEdges = np.histogram(data2[index1], bins=bin[i])
+        bincenters = 0.5 * (binEdges[1:] + binEdges[:-1])
+        plt.plot(bincenters,y,'-',linewidth=3)
+        ax.set_xlim(xlim)
+        ymax = ax.get_ylim()
+        ax.text(index2, ymax[1]*0.8, label[i], fontsize=15)
+        ax.legend()
+
+
+
+
+    #ax.legend(loc=7)
+    ax.set_xlabel(xtitle)
+    a = plot()
+    a.save(fig=fig, save=save)
+
+"Overall speed distribution for HCl"
+file1 = 'j=4.prn'
+file2 = ['./result_hcl/result_H_WWW.s1','./result_hcl/result_H_WWW.s2','./result_hcl/result_H_WWW.s3','./result_hcl/result_H_WWW.s4']
+
+save = 'HCl_internal.eps'
+xlim=[0,7000]
+xtitle='Velocity (m/s)'
+ytitle1='Experiment signal intensity'
+ytitle2='Trajectory count'
+linewidth=1
+bin = [100, 100, 100, 20]
+xtitle='HCl Internal Energy (cm$^{-1}$)'
+label = ['No Constraint','Hard ZPE on HCl','Soft ZPE on both','Hard ZPE on both']
+#joverallspped_plot2(file1,file2,save,xlim,xtitle,ytitle1,ytitle2,label, index1=2, index2 = 4000)
+xlim=[10000,20000]
+bin = [100, 100, 100, 50]
+save = 'WWW_internal.eps'
+xtitle='(H$_2$O)$_3$ Internal Energy (cm$^{-1}$)'
+#j_plot2(file1,file2,save,xlim,xtitle,ytitle1,ytitle2,label, index1=3, index2=10150)
+
