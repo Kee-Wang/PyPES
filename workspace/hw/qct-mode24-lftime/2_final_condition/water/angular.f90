8da1ec17b0f2aab467eba4a9b91ef6c8200424b4
Kee-Wang <kee@macronode.chem.emory.edu>
2018-04-12 20:08:08 -0400

    all added

---
+++ workspace/hw/qct-mode24-lftime/2_final_condition/water/angular.f90
@@ -0,0 +1,421 @@
+module angular
+  implicit none
+  
+contains
+
+  subroutine rotn(iflag,n,Q,v,m,AM,EROT,womega,abc,evect)
+    implicit none
+    integer,intent(in) :: iflag,n
+    real(kind=8),intent(in) :: v(n,3),Q(n,3),m(n)
+    real(kind=8),intent(inout) :: AM(3)
+    real(kind=8),intent(out) :: EROT
+    real(kind=8),intent(out) :: womega(3)
+    ! ::::::::::::::::::::::::::::
+    integer :: i,j,k,lwork,info
+    real(kind=8) :: moi(3,3),moi_inv(3,3),AM4,P(n,3)
+    real(kind=8) :: xl(3,n),x0(n,3)
+    real(kind=8) :: tmas,det
+    real(kind=8) :: abc(3)!To store const ABC for polyatomic mol
+    real(kind=8) :: eig(3),work(1:40*size(eig)),evect(3,3)
+    real(kind=8) :: sr,dum,cbmax,bmax,detcb1,detcb2,detcb3,detcb,convert
+    real(kind=8) :: atoau,wavenm,pi,EROTC(2),w_abc(3),rconst(3)
+    real(kind=8) :: WX,WY,WZ
+    character(len=100) :: formatstring
+    
+abc = 0
+!   write(*,*) 'iflag =',iflag
+!   write(*,*) 'n =',n
+!   write(*,*) 'Q =', Q
+!   write(*,*) 'v =', v
+!   write(*,*) 'm =', m
+
+    atoau=1.66053886d-27/9.1093826d-31
+    wavenm=2.1947463067d+05 !! multiply by this to convert Hartree to cm-1
+    lwork=40*size(eig)
+    pi = dacos(-1D0)
+    rconst=0.0d0
+    
+    if(iflag.eq.0) then
+       AM=0.0d0
+       EROT=0.0d0
+       if(n.eq.1) return
+       do i=1,n
+          P(i,1:3) = m(i)*v(i,1:3)
+          AM(1) =  AM(1) +  (Q(i,2)*P(i,3)-Q(i,3)*P(i,2))
+          AM(2) =  AM(2) +  (Q(i,3)*P(i,1)-Q(i,1)*P(i,3))
+          AM(3) =  AM(3) +  (Q(i,1)*P(i,2)-Q(i,2)*P(i,1))
+       end do
+       AM4 = DSQRT(AM(1)**2+AM(2)**2+AM(3)**2)
+       if(n.eq.2) then
+          moi(1,1)=0.0d0
+          do i=1,n
+            moi(1,1)=moi(1,1)+(Q(i,1)**2+Q(i,2)**2+Q(i,3)**2)*m(i)
+          end do
+       EROT=AM4**2/moi(1,1)/2.0d0
+
+       return
+       end if !! n.eq.2
+    end if  !! iflag.eq.0
+    
+    moi=0.0d0
+    moi_inv=0.0d0
+    !! construct moment of inertia
+    do i=1,n
+     tmas=m(i)
+     moi(1,1) = moi(1,1) + tmas*(Q(i,2)**2+Q(i,3)**2)
+     moi(2,2) = moi(2,2) + tmas*(Q(i,1)**2+Q(i,3)**2)
+     moi(3,3) = moi(3,3) + tmas*(Q(i,2)**2+Q(i,1)**2)
+     moi(1,2) = moi(1,2) - tmas*(Q(i,1)*Q(i,2))
+     moi(2,3) = moi(2,3) - tmas*(Q(i,2)*Q(i,3))
+     moi(1,3) = moi(1,3) - tmas*(Q(i,1)*Q(i,3))
+    end do
+     moi(2,1)=moi(1,2)
+     moi(3,2)=moi(2,3)
+     moi(3,1)=moi(1,3)
+    
+    !! save moment of inertia
+    evect=moi
+    !! diagonalize moment of inertia matrix
+    call dsyev('v','u',3,evect,3,eig,work,lwork,info)
+    if(info.ne.0) stop 'Moment of intertia diagonalization failed'
+    
+    !! calculate determinant
+          det = ( moi(1,1)*moi(2,2)*moi(3,3) + &
+                  moi(1,2)*moi(2,3)*moi(3,1) + &
+                  moi(1,3)*moi(2,1)*moi(3,2) ) - &
+                ( moi(3,1)*moi(2,2)*moi(1,3) + &
+                  moi(3,2)*moi(2,3)*moi(1,1) + &
+                  moi(3,3)*moi(2,1)*moi(1,2) )
+    
+    !! calculate inverse of the intertia tensor
+         if(abs(det).ge.0.001d0) then
+                moi_inv(1,1) =  (moi(2,2)*moi(3,3)-moi(2,3)*moi(3,2))/det
+                moi_inv(1,2) =  (moi(1,3)*moi(3,2)-moi(1,2)*moi(3,3))/det
+                moi_inv(1,3) =  (moi(1,2)*moi(2,3)-moi(1,3)*moi(2,2))/det
+    
+                moi_inv(2,1) =  (moi(2,3)*moi(3,1)-moi(2,1)*moi(3,3))/det
+                moi_inv(2,2) =  (moi(1,1)*moi(3,3)-moi(1,3)*moi(3,1))/det
+                moi_inv(2,3) =  (moi(1,3)*moi(2,1)-moi(1,1)*moi(2,3))/det
+    
+                moi_inv(3,1) =  (moi(2,1)*moi(3,2)-moi(2,2)*moi(3,1))/det
+                moi_inv(3,2) =  (moi(1,2)*moi(3,1)-moi(1,1)*moi(3,2))/det
+                moi_inv(3,3) =  (moi(1,1)*moi(2,2)-moi(1,2)*moi(2,1))/det
+    
+    !! angular velocity
+                womega(1:3) = matmul(moi_inv,AM(1:3))
+    
+                WX=moi_inv(1,1)*AM(1)+moi_inv(1,2)*AM(2)+moi_inv(1,3)*AM(3)
+                WY=moi_inv(2,1)*AM(1)+moi_inv(2,2)*AM(2)+moi_inv(2,3)*AM(3)
+                WZ=moi_inv(3,1)*AM(1)+moi_inv(3,2)*AM(2)+moi_inv(3,3)*AM(3)
+    
+                w_abc(1:3)=matmul(womega,evect)
+          else
+    !! calculate rotational energy 
+                womega(1:3)=0.0d0
+                if(moi(1,1).ne.0.0) womega(1)=AM(1)/moi(1,1)
+                if(moi(2,2).ne.0.0) womega(2)=AM(2)/moi(2,2)
+                if(moi(3,3).ne.0.0) womega(3)=AM(3)/moi(3,3)
+                moi(1,1)=0.0d0
+             do i=1,n
+               sr=0.0d0
+               do j=1,3
+                  sr=sr+Q(i,j)*Q(i,j)
+               end do
+                  moi(1,1)=moi(1,1)+sr*m(i)
+             end do
+             AM4=DSQRT(AM(1)**2+AM(2)**2+AM(3)**2)
+             EROT=AM4**2/moi(1,1)/2.0d0
+            return
+          end if
+    
+     EROT=(womega(1)*AM(1)+womega(2)*AM(2)+womega(3)*AM(3))/2.0d0
+    
+     EROTC(1)=(eig(1)*w_abc(1)**2+eig(2)*w_abc(2)**2)/2.0
+     EROTC(2)=(eig(3)*w_abc(3)**2)/2.0
+
+abc(:) = 0.5/eig(:)
+!write(*,*) evect
+! write(*,*) 'Eigenvec'
+!write(*,*) evect
+!write(*,*) 'Eigenvalue',eig,abc
+!write(*,*) 'Verify by evect*I*transpose(evect) = eigenvlue'
+!write(*,*) matmul(transpose(evect),matmul(moi,evect))
+    return
+  end subroutine rotn
+
+  !*****************************************************!
+  ! Eckart frame       JCP, 122, 124103 (2005)          !
+  !*****************************************************!
+  subroutine eckart_coor(natom,rr,refStruc,mass,vv,tmin)
+    double precision,parameter::threshold=0.25d0
+    integer,intent(in)::natom
+    double precision,dimension(3,natom),intent(inout)::rr,vv
+    double precision,dimension(3,natom),intent(in)::refStruc
+    double precision,dimension(natom),intent(in)::mass
+    double precision,dimension(3,3),intent(out)::tmin
+    !::::::::::::::::::::::::::::::::::::::::::::::::
+    integer::i,j,k,ierr,i1,i2,i3
+    double precision::totMass,temp,dxmin
+    double precision,dimension(3,3)::a,a1,a2,u,v,t,vTemp
+    double precision,dimension(3,natom)::rrTemp,vvTemp,rrmin,vvmin
+    double precision,dimension(3)::d,e
+  
+    ! total mass of the system
+    totMass = 0.d0
+    do i = 1, natom
+       totMass = totMass + mass(i)
+    end do
+  
+    ! shifting the origin into the nuclear center of mass
+    do i = 1, 3
+       temp = 0.d0
+       do j = 1, natom
+          temp = temp + mass(j) * rr(i,j)
+       end do
+       temp = temp/totMass
+       do j = 1, natom
+          rr(i,j) = rr(i,j) - temp
+       end do
+    end do
+  
+    ! constructing a, a1 and a2 matrices
+    do i = 1, 3
+       do j = 1, 3
+          temp = 0.d0
+          do k = 1, natom
+             temp = temp + mass(k) * rr(i,k) * refStruc(j,k)
+          end do
+          a(i,j) = temp
+       end do
+    end do
+  
+    do i = 1, 3
+       do j = i, 3
+          temp = 0.d0
+          do k = 1, 3
+             temp = temp + a(i,k) * a(j,k)
+          end do
+          a1(i,j) = temp
+          a1(j,i) = temp
+          temp = 0.d0
+          do k = 1, 3
+             temp = temp + a(k,i) * a(k,j)
+          end do
+          a2(i,j) = temp
+          a2(j,i) = temp
+       end do
+    end do
+  
+    ! a1 and a2 eigenproblems
+    call subtred2(3,3,a1,d,e,u)
+    call tql2(3,3,d,e,u,ierr)
+    call subtred2(3,3,a2,d,e,v)
+    call tql2(3,3,d,e,v,ierr)
+
+    dxmin = 1000000.0d0 
+    rrTemp = rr
+    vvTemp = vv
+    vTemp = v
+    do i1 = 1, 2
+       do i2 = 1, 2
+          do i3 = 1, 2
+
+             do j = 1, 3
+                v(j,1) = (-1.d0)**i1*vTemp(j,1)
+                v(j,2) = (-1.d0)**i2*vTemp(j,2)
+                v(j,3) = (-1.d0)**i3*vTemp(j,3)
+             end do
+  
+             ! constructing t
+             do i = 1, 3
+                do j = 1, 3
+                   temp = 0.d0
+                   do k = 1, 3
+                      temp = temp + v(i,k) * u(j,k)
+                   end do
+                   t(i,j) = temp
+                end do
+             end do
+  
+             ! rotating the Descartes position vectors of the nuclei
+!             rrTemp = rr
+             do i = 1, 3
+                do j = 1, natom
+                   temp = 0.d0
+                   do k = 1, 3
+                      temp = temp + t(i,k) * rrTemp(k,j)
+                   end do
+                   rr(i,j) = temp
+                end do
+             end do
+  
+!             vvTemp = vv
+             do i = 1, 3
+                do j = 1, natom
+                   temp = 0.d0
+                   do k = 1, 3
+                      temp = temp + t(i,k) * vvTemp(k,j)
+                   end do
+                   vv(i,j) = temp
+                end do
+             end do
+  
+             ! testing the rotational Eckart conditions
+!             temp = 0.d0
+!             do j = 1, natom
+!                temp = temp + mass(j) * (rr(2,j) * refStruc(3,j) - &
+!                       rr(3,j) * refStruc(2,j))
+!             end do
+!             if (dabs(temp) > threshold) then
+!                write(*,*) 'x error', temp
+!             end if
+!
+!             temp = 0.d0
+!             do j = 1, natom
+!                temp = temp + mass(j) * (rr(3,j) * refStruc(1,j) - &
+!                       rr(1,j) * refStruc(3,j))
+!             end do
+!             if (dabs(temp) > threshold) then
+!                write(*,*) 'y error', temp
+!             end if
+!
+!             temp = 0.d0
+!             do j = 1, natom
+!                temp = temp + mass(j) * (rr(1,j) * refStruc(2,j) - &
+!                       rr(2,j)*refStruc(1,j))
+!             end do
+!             if (dabs(temp)>threshold) then
+!                write(*,*) 'z error', temp
+!             end if
+  
+             ! computing rr - ref
+             temp = 0.0d0
+             do i = 1, 3
+                do j = 1, natom
+                   temp = temp + (rr(i,j) - refStruc(i,j))**2
+                end do
+             end do
+             if (temp < dxmin) then
+                dxmin = temp  
+                rrmin = rr
+                vvmin = vv
+                tmin = t 
+             end if
+  
+          end do
+       end do
+    end do
+   
+    rr = rrmin
+    vv = vvmin
+  
+    !determinant of the transformation matrix  
+!    det=tmin(1,1)*(tmin(2,2)*tmin(3,3)-tmin(2,3)*tmin(3,2))
+!    det=det-tmin(1,2)*(tmin(2,1)*tmin(3,3)-tmin(2,3)*tmin(3,1))
+!    det=det+tmin(1,3)*(tmin(2,1)*tmin(3,2)-tmin(2,2)*tmin(3,1)) 
+  
+    return
+  end subroutine eckart_coor
+
+  !=======================================!
+  ! diagnolize the interial tensor        !
+  ! and set J=0                           !
+  !=======================================!
+  subroutine diag_it(mass,x,v)
+    real,dimension(:),intent(in)::mass
+    real,dimension(:),intent(inout)::x
+    real,dimension(:),intent(inout)::v
+    ! ::::::::::::::::::::
+    real,dimension(1:3,1:3)::it
+    integer::i,n
+    real::tmas
+    real,dimension(1:3)::cord,velc,J,omega
+    integer::info,lwork
+    real,dimension(1:3)::egvu !Eigen Vaule
+    real,dimension(1:12)::work !just temporary space
+    lwork=12
+    
+    n=size(mass,1)
+    
+    it=0    
+    do i=1,n
+       tmas = mass(i)
+       cord = x(3*i-2:3*i)
+       it(1,1) = it(1,1) + tmas*( cord(2)**2 + cord(3)**2 )
+       it(2,2) = it(2,2) + tmas*( cord(1)**2 + cord(3)**2 )
+       it(3,3) = it(3,3) + tmas*( cord(2)**2 + cord(1)**2 )
+       it(1,2) = it(1,2) - tmas*( cord(1)*cord(2) )
+       it(2,3) = it(2,3) - tmas*( cord(2)*cord(3) )
+       it(1,3) = it(1,3) - tmas*( cord(1)*cord(3) )
+    end do
+
+    it(2,1)=it(1,2)
+    it(3,2)=it(2,3)
+    it(3,1)=it(1,3)
+
+    call dsyev('v','u',3,it,3,egvu,work,lwork,info)
+   
+    do i=1,n
+       cord = x(3*i-2:3*i)
+       x(3*i-2:3*i) = matmul(transpose(it),cord)
+       velc = v(3*i-2:3*i)
+       v(3*i-2:3*i) = matmul(transpose(it),velc)
+    end do
+
+    !================!
+    ! set J=0        !
+    !================!
+    J=0; velc=0; omega=0; cord=0
+    
+    do i=1,n
+       tmas = mass(i)
+       cord = x(3*i-2:3*i)
+       velc = v(3*i-2:3*i)
+       
+       J(1) = J(1) + tmas*(cord(2)*velc(3)-cord(3)*velc(2))
+       J(2) = J(2) + tmas*(cord(3)*velc(1)-cord(1)*velc(3))
+       J(3) = J(3) + tmas*(cord(1)*velc(2)-cord(2)*velc(1))
+    end do
+    
+    omega=J/egvu
+
+    do i=1,n
+       cord = x(3*i-2:3*i)
+       velc = v(3*i-2:3*i)
+       v(3*i-2) = velc(1) + cord(2)*omega(3) - cord(3)*omega(2)
+       v(3*i-1) = velc(2) + cord(3)*omega(1) - cord(1)*omega(3)
+       v(3*i)   = velc(3) + cord(1)*omega(2) - cord(2)*omega(1)
+    end do
+
+    return
+  end subroutine diag_it
+
+  !=====================================!
+  ! Calculate the angular momentum      !
+  !=====================================!
+  subroutine monitor_J(mass,x,v)
+    real,dimension(:),intent(in)::mass,x,v
+!    real,dimension(1;3),intent(out)::J
+    !:::::::::::::::::::
+    real,dimension(1:3)::xx,vv,J
+    integer::natm,i
+    real::tmas
+
+    natm = size(mass)
+
+    J = 0.0
+    do i=1,natm
+       tmas = mass(i)
+       xx = x(3*i-2:3*i)
+       vv = v(3*i-2:3*i)
+       
+       J(1) = J(1) + tmas*(xx(2)*vv(3)-xx(3)*vv(2))
+       J(2) = J(2) + tmas*(xx(3)*vv(1)-xx(1)*vv(3))
+       J(3) = J(3) + tmas*(xx(1)*vv(2)-xx(2)*vv(1))
+    end do
+
+    write(15,'(3F15.8)') J
+
+  end subroutine
+
+end module angular
---
+++ workspace/hw/qct-mode24-lftime/2_final_condition/water/getpot.f90
@@ -0,0 +1,308 @@
+program main
+! For water
+! and ZPE will be checked.
+
+use final_condition
+  use pes_shell
+  use constants
+  use angular
+  implicit none
+real,parameter::Be_HCl=10.59341
+real,parameter::aums=2187695.176776!1au = aums m/s
+!ZPE calcualted use our own PES
+!HO ZPE using John's calcualtion
+real,parameter::zpe_w= 4637.098762 !HO=4713.0629051 !DMC = 4637.098762
+real,parameter::zpe_hww= 12220.164040! HO =12528.93462455 !DMC = 12220.164040
+real,parameter::zpe_hwww = 17683.110522 !DMC = 17683.110522
+real,parameter::E_w_ref =8462.8673097354421770 !Ref to min
+real,parameter::E_hww_ref = 3537.7663972273066975 !Ref to min
+real,parameter::E_given =  21233.1103893624 !Total energy
+real,dimension(:,:),allocatable::xx,gradd,velo
+character(len=2),dimension(:),allocatable::sym
+character(len=2),dimension(3)::sym_w
+character(len=32)::filename
+integer::k,i,natm,ierr,j,vio, ii
+real::pot,diss, E_trans,E_T, E_V, M_w, E_rot, speed, Evib
+real::Evib_hww, speed_hww
+real,dimension(:),allocatable::mass
+real,dimension(:),allocatable::x
+real,dimension(9)::x_w
+real,dimension(9)::v_w
+real,dimension(24)::x_hww
+real,dimension(24)::v_hww
+real,dimension(3,3)::xxw
+integer::j221c,j321c, D0count(4), iter, ab_j
+real:: D0, red_w, red_hww, kin_hwww, x_hwww(33), v_hwww(33), com_velc(3)
+real:: Ekine, Ekine_hww
+
+!For H2O fragment
+real,dimension(3)::mass_w !Redefine mass for w
+real,dimension(8)::mass_hww !Redefine mass for w
+real,dimension(3)::abc,abc_hww
+
+real,dimension(3,3)::w_r, w_v !Coordiante for w and velocity for w
+real,dimension(3,3)::w_r_prime, w_v_prime !Coordiante and velocity in com
+
+real,dimension(8,3)::hww_r, hww_v !Coordiante for w and velocity for w
+real,dimension(8,3)::hww_r_prime, hww_v_prime !Coordiante and velocity in com
+
+real,dimension(3)::w_com_r, w_com_v!Vectos to define com coord and velocity
+real,dimension(3)::j_w, omega !Angular momentum of HCl
+
+real,dimension(3)::hww_com_r, hww_com_v!Vectos to define com coord and velocity
+real,dimension(3)::j_hww, omega_hww !Angular momentum of HWW
+
+real,dimension(3,3)::xxx,vv,evect
+real,dimension(3,8)::xxhww
+real::Erot
+real::Erot_hww
+integer:: iflag(4), jmoi(3), jall,jsum(4),jcount(4),jac(3)
+real::J1(3),J1_hww(3),vsum(4), Jall_real_sum(4)
+real::Jall_real,jmoi_real(3)
+real::pot_w, pot_hww, pot_hwww
+
+  call getarg(1,filename)
+open(19, status='old',file='watpot/result_HWW_W.wat') !Read water potential
+open(18, status='old',file='hwwpot/result_HWW_W.hww') !Read HWW potential
+  open(21,status='old',file=filename)
+  i=index(filename,'.',.true.)
+  filename=filename(1:i-1)
+
+!These files are for recording
+  open(34,status='unknown',file=trim(filename)//"_s4_j221.geom")!Hard restriction
+  open(22,status='unknown',file=trim(filename)//"_s1.txt")!No restriction
+  open(23,status='unknown',file=trim(filename)//"_s2.txt")!water restriction
+  open(24,status='unknown',file=trim(filename)//"_s3.txt")!Soft restriction
+  open(25,status='unknown',file=trim(filename)//"_s4.txt")!Hard restriction
+
+  open(26,status='unknown',file=trim(filename)//"_s1_j221.txt")!J=221
+  open(27,status='unknown',file=trim(filename)//"_s2_j221.txt")
+  open(28,status='unknown',file=trim(filename)//"_s3_j221.txt")
+  open(29,status='unknown',file=trim(filename)//"_s4_j221.txt")
+  open(30,status='unknown',file=trim(filename)//"_s1_j321.txt")!J=321
+  open(31,status='unknown',file=trim(filename)//"_s2_j321.txt")
+  open(32,status='unknown',file=trim(filename)//"_s3_j321.txt")
+  open(33,status='unknown',file=trim(filename)//"_s4_j321.txt")
+
+write(22,'(3A15)')  '#No_Constraint','#No_Constraint','#No_Constraint'
+write(23,'(3A15)')  '#Hard_water','#Hard_water','#Hard_water'
+write(24,'(3A15)')  '#Soft_ZPE', '#Soft_ZPE', '#Soft_ZPE'
+write(25,'(3A15)')  '#Hard_ZPE','#Hard_ZPE','#Hard_ZPE'
+
+write(26,'(3A15)')  '#No_Constraint','#No_Constraint','#No_Constraint'
+write(27,'(3A15)')  '#Hard_water','#Hard_water','#Hard_water'
+write(28,'(3A15)')  '#Soft_ZPE', '#Soft_ZPE', '#Soft_ZPE'
+write(29,'(3A15)')  '#Hard_ZPE','#Hard_ZPE','#Hard_ZPE'
+
+write(30,'(3A15)')  '#No_Constraint','#No_Constraint','#No_Constraint'
+write(31,'(3A15)')  '#Hard_water','#Hard_water','#Hard_water'
+write(32,'(3A15)')  '#Soft_ZPE', '#Soft_ZPE', '#Soft_ZPE'
+write(33,'(3A15)')  '#Hard_ZPE','#Hard_ZPE','#Hard_ZPE'
+
+do j=1,12
+!write(21+j,*) '#Water ZPE (DMC): 4713.0629051, HWW ZPE (DMC): 12528.93462455'
+!write(21+j,*) '#E_given = DMC ZPE + 3550 =21233.1103893624.'
+!write(21+j,*) '#Dvib = Evib - ZPE'
+!write(21+j,*) "#D0 = E_given - Erot(W) - Erot(HWW) - Evib(W) + Evib(HWW) - E_COM,&
+! E_COM=0"
+!write(21+j,*)
+
+write(21+j,'(3A15)')  '#Erot(W)cm-1','#J',  '#Speed(m/s)'
+!write(21+j,'(7A15,A2)')  '#Erot(W)','#Erot(HWW)',&
+!'#Dvib(W)', '#Dvib(HWW)','D0', '#Speed'!,'#J'
+end do
+
+
+!Process begins here
+!initialize
+Jall_real_sum = 0
+D0count = 0
+
+  read(21,*) natm
+  read(21,*) 
+
+  allocate(xx(3,natm))
+  allocate(gradd(3,natm))
+  allocate(velo(3,natm))
+  allocate(sym(natm))
+  allocate(mass(natm))
+  allocate(x(natm*3))
+
+  do i=1,natm
+     read(21,*) sym(i),xx(:,i),gradd(:,i),velo(:,i)
+       select case(sym(i))
+       case("C")
+          mass(i)=c_mass
+       case("H")
+          mass(i)=h_mass
+       case("O")
+          mass(i)=o_mass
+       case("Cl")
+          mass(i)=cl_mass
+       end select
+    end do
+  rewind(21)
+mass = mass*emass
+mass_w(1:2) = mass(1:2)
+mass_w(3) = mass(7)
+mass_hww(1:4) = mass(3:6)
+mass_hww(5:8) = mass(8:11)
+
+  call clust_break(sym)
+  call pes_init()
+
+
+
+!Loop throught all configurations
+
+  k = 0!record total number of configs
+do
+     read(21,*,iostat=ierr) natm
+     if (ierr.ne.0) exit
+     k = k + 1
+     read(21,*)
+     do i=1,natm
+        read(21,*) sym(i),xx(:,i),gradd(:,i),velo(:,i)
+     end do
+     xx = xx/auang !xx in Bohr
+
+!For whole HWWW, Flaten to 1D
+do j=1,11
+x_hwww(3*j-2:3*j) = xx(:,j)
+v_hwww(3*j-2:3*j) = velo(:,j)
+end do
+pot_hwww = f(xx) * aucm
+!D0 = f(xx) * aucm + calc_kine(mass,v_hwww)*aucm
+!write(*,*) D0
+!Total energy using full potential!Checked this is conserved
+
+!Kinetic energy of HWWW, verify this hould be 0
+com_velc=0
+    do i=1,natm
+       com_velc=com_velc+mass(i)*v_hwww(3*i-2:3*i)
+    end do
+com_velc=com_velc/sum(mass)
+!kin_hwww = calc_kine(mass,com_velc)
+kin_hwww = calc_kine(mass,v_hwww)*aucm
+call xcom(mass,x_hwww)
+call vcom(mass,v_hwww)
+
+!Assign cooridnate and velocity to w, as 1d
+!For W
+do j=1,2
+x_w(3*j-2:3*j) = x_hwww(3*j-2:3*j)
+v_w(3*j-2:3*j) = v_hwww(3*j-2:3*j)
+end do
+j=3
+x_w(3*j-2:3*j) = x_hwww(3*7-2:3*7)
+v_w(3*j-2:3*j) = v_hwww(3*7-2:3*7)
+!For HWW
+do j=1,4
+x_hww(3*j-2:3*j) = x_hwww(3*(j+2)-2:3*(j+2))
+v_hww(3*j-2:3*j) = v_hwww(3*(j+2)-2:3*(j+2))
+x_hww(3*(j+4)-2:3*(j+4)) =  x_hwww(3*(j+7)-2:3*(j+7))
+v_hww(3*(j+4)-2:3*(j+4)) =  v_hwww(3*(j+7)-2:3*(j+7))
+end do
+
+!To this point, x_w, x_hww, v_w, v_www are all in COM frame.
+
+!Calculate rotational energy Erot and translational energy Ekine
+!For water
+call fin_cond(mass_w,x_w,v_w,speed,Ekine,Erot,j_w,abc,evect)
+ab_j = nint(sqrt(0.25 + sum(j_w**2))-0.5)
+call fin_cond(mass_hww,x_hww,v_hww,speed_hww,Ekine_hww,Erot_hww,j_hww,abc_hww,evect)
+
+!calculate vibrational energy, so that to determin if zpe violated
+read(19,*) pot_w
+Evib = calc_kine(mass_w,v_w)- Erot 
+Evib = Evib*aucm + pot_w 
+
+read(18,*) pot_hww
+Evib_hww = calc_kine(mass_hww,v_hww)- Erot_hww
+Evib_hww = Evib_hww*aucm + pot_hww 
+
+
+
+
+!Classification for different ZPE condition
+iflag = 0
+iflag(1) = 1
+if (Evib > zpe_w) then !Only water
+iflag(2) = 1
+end if
+if (Evib + Evib_hww > zpe_hww + zpe_w) then !Soft ZPE
+iflag(3) = 1
+end if
+if (Evib > zpe_w .and. Evib_hww > zpe_hww) then !Hard ZPE
+iflag(4) = 1
+
+
+end if
+
+!Rotational energy
+Erot = Erot * aucm
+Erot_hww = Erot_hww * aucm
+
+!Excessive vibrational energy, red=redundent
+red_hww = Evib_hww - zpe_hww
+red_w = Evib - zpe_w
+
+
+!D0 according to current result
+!De
+!D0 = (E_given) - (Erot + Erot_hww) - (Evib + Evib_hww)&
+!-(Ekine + Ekine_hww)
+
+D0 =  pot_hwww - pot_w - pot_hww
+!D0 = E_given - ( kin_hwww + pot_w + pot_hww)
+
+
+do i = 1,4 !Record according to different ZPE conditon
+
+if (iflag(i) .eq. 1)  then
+!For J221
+if ((abs(ab_j-2.0) <=1d-5) .and. (115.669905 < Erot) .and. (Erot < 135.532765) )then
+write(25+i,'(F15.2,I15,F15.2)')  Erot,ab_j, speed*aums
+!Print ZPE configs
+ if (iflag(4) .eq. 1) then
+write(*,*) 'found',k
+write(34,*) 11
+write(34,*) k
+  do ii=1,natm
+    write(34,*) sym(ii),xx(:,ii)*auang
+  end do
+end if
+ end if
+!For J321
+if ((abs(ab_j-3.0) <=1d-5) .and. (209.22888 < Erot) .and. (Erot < 248.78747) )then
+write(29+i,'(F15.2,I15,F15.2)')  Erot,ab_j, speed*aums
+end if
+
+
+Jsum(i) = Jsum(i) + jall
+Jcount(i) = Jcount(i) + 1
+vsum(i) = vsum(i) + speed*aums
+Jall_real_sum(i) = Jall_real_sum(i) + Jall_real
+D0count(i) = D0count(i) + D0
+
+write(21+i,'(F15.2,I15,F15.2)')  Erot,ab_j, speed*aums
+!write(21+i,'(6(F15.2))')  Erot, Erot_hww, &
+!Evib - zpe_w, Evib_hww-zpe_hww, D0, speed*aums
+
+
+
+
+end if
+end do
+
+
+  enddo
+
+write(*,*) 'Jcount,no/hard on hcl/soft/hard',Jcount
+write(*,'(A20,4F8.2)') 'Rejection Rate:',1-real(Jcount)/real(Jcount(1))
+write(*,'(A20,4F8.2)') 'Average J: ',Jsum(:)/real(Jcount(:))
+write(*,'(A20,4F8.2)') 'Average Speed (m/s): ',vsum(:)/real(Jcount(:))
+write(*,'(A20,4F8.2)') 'Average D0 (cm-1): ',D0count(:)/real(Jcount(:))
+
+
+end program main
